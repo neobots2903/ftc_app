1c83922815ce1bd3ddb5838ad90b17bef2c3ea15
Tammo Koonen <tammokoonen@gmail.com>
2019-09-27 17:36:36 -0700

    Added hardwareMap

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware9330.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Hardware9330 {
+    HardwareMap hwMap = null;
+
+    public static DcMotor rightFront;
+    public static DcMotor leftFront;
+    public static DcMotor rightBack;
+    public static DcMotor leftBack;
+
+}
